8b74a320acb569e68976c744e0b29f5e2d5c96f0
Pacchiotto <marcopac.363.d@gmail.com>
2023-07-01 22:46:04 +0200

    gobuster

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ReMakeingToolsToLearn
+
 ReMakeing Tools To Learn(it) how it it's work
+
+## Nmap
+
+![nmap](img/nmap.png)
+
+## GoBuster
+
+![gobuster](img/gobuster.png)
+
+## BurpSuite
+
+## Jhon The Ripple
+
+## Stupid names
+
+    AppleGuard
+    MangoShield
+    BlueberrySecure
+    KiwiDefender
+    PineappleFirewall
+    BananaEncryption
+    GrapePhishing
+    OrangeMalware
+    CherryVPN
+    LemonBotnet
---
+++ ReMake_GoBuster/RM_GoBuster.py
@@ -0,0 +1,35 @@
+a = """
+▄▄▄  ▄▄▄ .• ▌ ▄ ·.  ▄▄▄· ▄ •▄ ▪   ▐ ▄  ▄▄ •
+▀▄ █·▀▄.▀··██ ▐███▪▐█ ▀█ █▌▄▌▪██ •█▌▐█▐█ ▀ ▪
+▐▀▀▄ ▐▀▀▪▄▐█ ▌▐▌▐█·▄█▀▀█ ▐▀▀▄·▐█·▐█▐▐▌▄█ ▀█▄
+▐█•█▌▐█▄▄▌██ ██▌▐█▌▐█▪ ▐▌▐█.█▌▐█▌██▐█▌▐█▄▪▐█
+.▀  ▀ ▀▀▀ ▀▀  █▪▀▀▀ ▀  ▀ ·▀  ▀▀▀▀▀▀ █▪·▀▀▀▀
+
+ ▄▄ •       ▄▄▄▄· ▄• ▄▌.▄▄ · ▄▄▄▄▄▄▄▄ .▄▄▄
+▐█ ▀ ▪ ▄█▀▄ ▐█ ▀█▪█▪██▌▐█ ▀. •██  ▀▄.▀·▀▄ █·
+▄█ ▀█▄▐█▌.▐▌▐█▀▀█▄█▌▐█▌▄▀▀▀█▄ ▐█.▪▐▀▀▪▄▐▀▀▄
+▐█▄▪▐█▐█▌.▐▌██▄▪▐█▐█▄█▌▐█▄▪▐█ ▐█▌·▐█▄▄▌▐█•█▌
+·▀▀▀▀  ▀█▄▀▪·▀▀▀▀  ▀▀▀  ▀▀▀▀  ▀▀▀  ▀▀▀ .▀  ▀
+
+"""
+# https://texteditor.com/multiline-text-art/
+import requests
+
+print(a)
+
+
+def scan_dir(url, wordlist):
+    with open(wordlist, "r") as f:
+        dirs = f.read().splitlines()
+
+    for directory in dirs:
+        dir_url = url + "/" + directory
+        response = requests.get(dir_url)
+        if response.status_code == 200:
+            print("[+] Found directory: " + dir_url)
+
+
+if __name__ == "__main__":
+    url = input("Enter the URL to scan: ")
+    wordlist = "ReMake_GoBuster\Wordlist.txt"
+    scan_dir(url, wordlist)
